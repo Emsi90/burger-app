18f08792553ba4ec561c91995c796092a9e285db
Marcin Chojnacki <marcinchojnacki00@gmail.com>
2018-09-26 01:11:50 +0200

    settings

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Auxi from '../../hoc/Auxi'
+
+const layout = ( props ) => {
+  return (
+    <Auxi>
+      <div>Toolbar, SideDrawer, Backdrop</div>
+      <main>
+        {props.children}
+      </main>
+    </Auxi>
+  );
+};
+
+export default layout;
